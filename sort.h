773672a233eaebf53bc8c17f270beee2ff5280ff
Vernon-444 <mt.vernon44@gmail.com>
2022-08-17 14:54:17 -0700

    testing 3

--- sort.h
+++ sort.h
@@ -30,4 +30,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+void lomutorules(int *array, size_t size, unsigned int low, unsigned int high);
+
 #endif
